1479ac023df3826f6f698016c24f41dcd8e2a38e
rpmartz <ryan@ryanpmartz.com>
2022-12-01 05:18:50 -0800

    refactor out most recursion

--- src/aoc_2022/day01.clj
+++ src/aoc_2022/day01.clj
@@ -2,30 +2,32 @@
   (:require [aoc-2022.common :as common]
             [clojure.string :as string]))
 
-(def puzzle-input
-  (let [raw-input (slurp "resources/day01.txt")]
-    (string/split raw-input #"\n")))
+(def per-elf-str-values
+  ;; puzzle input split by double newline, which separates each 
+  ;; elf's food values into a string with numeric values separated by \n
+  (string/split (slurp "resources/day01.txt") #"\n\n"))
 
-(defn calculate-calorie-values []
-  (loop [x (first puzzle-input)
-         xs puzzle-input
-         intermediate-vals []
-         final-vals []]
-    (if (empty? xs)
-      final-vals
-      (if (= x "")
-        (let [new-val (reduce + intermediate-vals)]
-          (recur (first xs) (rest xs) [] (conj final-vals new-val)))
-        (recur (first xs) (rest xs) (conj intermediate-vals (common/str->int x)) final-vals)))))
+(def per-elf-line-values
+  ;; puzzle input structured with each elf's calorie values
+  ;; represented as a collection of numeric strings
+  (map #(string/split % #"\n") per-elf-str-values))
+
+(def elf-calorie-values
+  ;; total value that each elf is carrying
+  (loop [line (first per-elf-line-values)
+         lines (rest per-elf-line-values)
+         acc []]
+    (if (empty? lines)
+      acc
+      (let [current-line-value (reduce + (map common/str->int line))]
+        (recur (first lines) (rest lines) (conj acc current-line-value))))))
 
 (defn part-1 []
-  (let [xs (calculate-calorie-values)]
-    (apply max xs)))
-
+  (apply max elf-calorie-values))
 
 (defn part-2 []
-  (let [xs (calculate-calorie-values)]
-    (reduce + (take 3 (reverse (sort xs))))))
+  (reduce + (take 3 (reverse (sort elf-calorie-values)))))
+
 
 (do
   (println (part-1))
