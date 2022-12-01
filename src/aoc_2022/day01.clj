d161c8611b596d7f953a90e0882514bfee4118d1
rpmartz <ryan@ryanpmartz.com>
2022-11-30 22:19:29 -0800

    day 1 part 2

--- src/aoc_2022/day01.clj
+++ src/aoc_2022/day01.clj
@@ -20,7 +20,25 @@
             (recur (rest xs) (first xs) max-so-far [])))
         (recur (rest xs) (first xs) max-so-far (conj current-holdings x))))))
 
+(defn calculate-calorie-values []
+  (loop [x (first puzzle-input)
+         xs puzzle-input
+         intermediate-vals []
+         final-vals []]
+    (if (empty? xs)
+      final-vals
+      (if (= x "")
+        (let [new-val (reduce + intermediate-vals)]
+          (recur (first xs) (rest xs) [] (conj final-vals new-val)))
+        (recur (first xs) (rest xs) (conj intermediate-vals (common/str->int x)) final-vals)))))
 
+(defn part-2 []
+  (let [xs (calculate-calorie-values)]
+    (reduce + (take 3 (reverse (sort xs))))))
+
+(do
+  (println (part-1))
+  (println (part-2)))
 
 
 
