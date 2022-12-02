d7d9d875ac6690daac61890f70e413c6453ea157
rpmartz <ryan@ryanpmartz.com>
2022-12-01 22:17:21 -0800

    day 2 part 2

--- src/aoc_2022/day02.clj
+++ src/aoc_2022/day02.clj
@@ -17,6 +17,22 @@
                    :draw 3
                    :loss 0})
 
+(def letters-to-outcomes {"X" :loss
+                          "Y" :draw
+                          "Z" :win})
+
+(defn determine-choice-for-outcome [op-choice outcome]
+  (cond
+    (= :rock op-choice) (cond (= :win outcome) :paper
+                              (= :loss outcome) :scissors
+                              (= :draw outcome) :rock)
+    (= :paper op-choice) (cond (= :win outcome) :scissors
+                               (= :loss outcome) :rock
+                               (= :draw outcome) :paper)
+    (= :scissors op-choice) (cond (= :win outcome) :rock
+                                  (= :loss outcome) :paper
+                                  (= :draw outcome) :scissors)))
+
 (defn judge-result [mine theirs]
   (cond
     (= :rock mine) (cond (= :rock theirs) :draw
@@ -37,11 +53,20 @@
      (get choice-score my-choice)
      (get result-score result))))
 
+(defn score-round-desired-result [input]
+  (let [opp-choice (get letter-map (first (string/split input #"\s")))
+        desired-outcome (get letters-to-outcomes (second (string/split input #"\s")))]
+    (+ (get result-score desired-outcome) (get choice-score (determine-choice-for-outcome opp-choice desired-outcome)))))
+
 (defn part-1 []
   (let [rounds (string/split (slurp "resources/day02.txt") #"\n")]
     (reduce + (map score-round rounds))))
 
+(defn part-2 []
+  (let [rounds (string/split (slurp "resources/day02.txt") #"\n")]
+    (reduce + (map score-round-desired-result rounds))))
 
 
 (do
-  (part-1))
+  (part-1)
+  (part-2))
