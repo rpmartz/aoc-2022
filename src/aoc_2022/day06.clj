b227a02be0bb2f4d0510e2f500439ea5da942d95
rpmartz <ryan@ryanpmartz.com>
2022-12-05 21:41:45 -0800

    day 6 part 2

--- src/aoc_2022/day06.clj
+++ src/aoc_2022/day06.clj
@@ -2,14 +2,16 @@
 
 (def input (slurp "resources/day06.txt"))
 
-(defn part-1 []
-  (loop [items (rest (partition 4 1 input))
-         window (first (partition 4 1 input))
-         acc 4]
+(defn part-1 [x]
+  (loop [items (rest (partition x 1 input))
+         window (first (partition x 1 input))
+         acc x]
 
-    (if (or (> acc 100000) (= 4 (count (set window))))
+    (if (= x (count (set window)))
       acc
       (recur (rest items) (first items) (inc acc)))))
 
 (do
-  (println (part-1)))
+  (println (part-1 4))
+  (println (part-1 14))
+  )
