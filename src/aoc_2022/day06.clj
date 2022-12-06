7136163f24cfcff7074520e41aad330fdd03a6b6
rpmartz <ryan@ryanpmartz.com>
2022-12-05 21:39:17 -0800

    day 6 pt 1

---
+++ src/aoc_2022/day06.clj
@@ -0,0 +1,16 @@
+(ns aoc-2022.day06)
+
+
+(def input (slurp "resources/day06.txt"))
+
+(defn part-1 []
+  (loop [items (rest (partition 4 1 input))
+         window (first (partition 4 1 input))
+         acc 4]
+
+    (if (or (> acc 100000) (= 4 (count (set window))))
+      acc
+      (recur (rest items) (first items) (inc acc)))))
+
+(do
+  (println (part-1)))
