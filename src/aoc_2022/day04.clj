54e5a80cf34e53f15fd8d3d2ba199efddcd4a888
rmartz <martzrp@gmail.com>
2022-12-07 21:01:05 -0800

    cleanup let bindings with destructuring

--- src/aoc_2022/day04.clj
+++ src/aoc_2022/day04.clj
@@ -5,31 +5,22 @@
 (def lines (string/split-lines (slurp "resources/day04.txt")))
 
 (defn parse-interval [s]
-  (let [numbers (string/split s #"-")
-        start (Integer/parseInt (first numbers))
-        end (Integer/parseInt (second numbers))]
-    (list start end)))
+  (let [[start end] (string/split s #"-")]
+    (list (Integer/parseInt start) (Integer/parseInt end))))
 
 (defn parse-intervals [pair]
-  (let [left-interval (parse-interval (first pair))
-        right-interval (parse-interval (second pair))]
-    (concat left-interval right-interval)))
+  (let [[left-interval right-interval] pair]
+    (concat (parse-interval left-interval) (parse-interval right-interval))))
 
 (defn overlaps-completely? [xs]
-  (let [lb (first xs)
-        le (second xs)
-        rb (first (drop 2 xs))
-        re (second (drop 2 xs))]
+  (let [[lb le rb re] xs]
     (cond
       (and (<= lb rb) (>= le re)) true
       (and (<= rb lb) (>= re le)) true
       :else false)))
 
 (defn overlaps-at-all? [xs]
-  (let [lb (first xs)
-        le (second xs)
-        rb (first (drop 2 xs))
-        re (second (drop 2 xs))]
+  (let [[lb le rb re] xs]
     (cond
       (and (<= lb rb) (>= le rb)) true
       (and (<= rb lb) (>= re lb)) true
