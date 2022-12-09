3094f563e45e550438999e66deaebd451f84d542
rpmartz <ryan@ryanpmartz.com>
2022-12-09 08:01:34 -0800

    first crack at move right

--- src/aoc_2022/day09.clj
+++ src/aoc_2022/day09.clj
@@ -8,6 +8,38 @@
   (let [[dir steps] (str/split move #" ")]
     {:dir (keyword dir) :steps (aoc/str->int steps)}))
 
+; map to track position of head and tail, as well as visited nodes
+(def initial-state {:head {:x 0 :y 0}
+                    :tail {:x 0 :y 0}
+                    :head-visited #{}
+                    :tail-visited #{}})
+
+(defn move-right [state]
+  (let [{head :head
+         tail :tail
+         hvisited :head-visited
+         tvisited :tail-visited} state 
+        ]
+    {:head {:x (inc (get head :x)) :y (get head :y)}
+     :tail head
+     :head-visited (conj hvisited {:x (inc (get head :x)) :y (get head :y)})
+     :tail-visited (conj tvisited head)}))
 
 
-(map parse-move input)
+(defn perform-move
+  "Updates `state` based on the result of `move`
+   
+   move is a dictionary with a :dir and :steps
+   state matches the structure of `initial-state
+   "
+  [{dir :dir n :steps} move
+   state])
+
+(defn part-1 []
+  (let [moves (map parse-move input)]))
+
+
+(conj #{"a" "b" "c"} "d")
+
+(do
+  (println (part-1)))
--- test/aoc_2022/day09_test.clj
+++ test/aoc_2022/day09_test.clj
@@ -6,3 +6,22 @@
 (deftest test-parsing-move
   (testing "Testing Parsing Lines to Moves"
     (is (parse-move "R 4") {:dir :R :steps 4})))
+
+(deftest test-moving-right
+  (testing "Test moving right"
+    (is (= {:head {:x 3 :y 3}
+            :tail {:x 2 :y 3}
+            :head-visited #{{:x 0 :y 3}
+                            {:x 1 :y 3}
+                            {:x 2 :y 3}
+                            {:x 3 :y 3}}
+            :tail-visited #{{:x 0 :y 3}
+                            {:x 1 :y 3}
+                            {:x 2 :y 3}}}
+           (move-right {:head {:x 2 :y 3}
+                        :tail {:x 1 :y 3}
+                        :head-visited #{{:x 0 :y 3}
+                                        {:x 1 :y 3}
+                                        {:x 2 :y 3}}
+                        :tail-visited #{{:x 0 :y 3}
+                                        {:x 1 :y 3}}})))))
