f11e904a4d7ae335c526512254bd558eb879e1bb
rpmartz <ryan@ryanpmartz.com>
2022-12-09 15:14:54 -0800

    convenince function for building a point

--- src/aoc_2022/common.clj
+++ src/aoc_2022/common.clj
@@ -1,5 +1,15 @@
-(ns aoc-2022.common)
-
+(ns aoc-2022.common
+  (:require [clojure.string :as str]))
 
 (defn str->int [s]
   (. Integer parseInt s))
+
+(defn lines
+  "Reads all of the lines in the file located at `path`"
+  [path]
+  (str/split-lines (slurp path)))
+
+(defn pnt
+  "Returns a map representing a 2d coordinate"
+  [x y]
+  {:x x :y y})
