488a25b16ab745f27812eb6a7076766b4814cc0d
rmartz <martzrp@gmail.com>
2022-12-11 22:32:54 -0800

    calculate sizes in day 7

--- src/aoc_2022/day07.clj
+++ src/aoc_2022/day07.clj
@@ -5,10 +5,28 @@
 
 (def lines (s/split-lines (slurp "resources/day07.txt")))
 
-(reduce (fn [[sizemap dirstack] line]
-          (match [(s/split line #" ")]
-            [["$" "cd" "/"]] [sizemap ["/"]] ; add root to stack
-            [["$" "cd" ".."]] [sizemap (pop dirstack)] ; pop dirr off stack
-            [["$" "cd" dirname]] [sizemap (conj dirstack dirname)] ; add current dir to stack
-            [(:or ["dir" _] ["$" "ls"])] [sizemap dirstack] ; ignore
-            [[filesize fname]] [sizemap dirstack])) [{} []] lines)
+(defn pathify [dirs]
+  (str "/" (s/join "/" (rest dirs))))
+
+(defn update-sizemap-with-file [fsize sizemap dirstack]
+  (loop [path dirstack
+         hm sizemap]
+    (if (empty? path)
+      hm
+      (recur (drop-last path) (merge-with + hm {(pathify path) fsize})))))
+
+(def directory-sizes 
+  (reduce (fn [[sizemap dirstack] line]
+            (match [(s/split line #" ")]
+              [["$" "cd" "/"]] [sizemap ["/"]] ; add root to stack
+              [["$" "cd" ".."]] [sizemap (pop dirstack)] ; pop dirr off stack
+              [["$" "cd" dirname]] [sizemap (conj dirstack dirname)] ; add current dir to stack
+              [(:or ["dir" _] ["$" "ls"])] [sizemap dirstack] ; ignore 
+              [[filesize _]] [(update-sizemap-with-file (parse-long filesize) sizemap dirstack) dirstack])) [{} []] lines))
+
+(s/join "/" (rest ["/" "zft" "zrtm" "vhdhn" "djfww" "zft"]))
+
+(pathify ["/"])
+(drop-last ["/"])
+
+(reduce + (filter #(<= % 100000) directory-sizes))
