75c9462ff26c4836218613d9138ef19119b34083
rpmartz <ryan@ryanpmartz.com>
2022-12-12 06:39:03 -0800

    day 7 part 2

--- src/aoc_2022/day07.clj
+++ src/aoc_2022/day07.clj
@@ -15,7 +15,7 @@
       hm
       (recur (drop-last path) (merge-with + hm {(pathify path) fsize})))))
 
-(def directory-sizes 
+(def directory-sizes
   (reduce (fn [[sizemap dirstack] line]
             (match [(s/split line #" ")]
               [["$" "cd" "/"]] [sizemap ["/"]] ; add root to stack
@@ -24,6 +24,22 @@
               [(:or ["dir" _] ["$" "ls"])] [sizemap dirstack] ; ignore 
               [[filesize _]] [(update-sizemap-with-file (parse-long filesize) sizemap dirstack) dirstack])) [{} []] lines))
 
-(let [part-1 (reduce + (filter #(<= % 100000) (vals (first directory-sizes))))]
-  (println (str "Part 1: " part-1)))
+(defn part-1
+  "Finds the sum of all the values of directories under 100000"
+  []
+  (reduce + (filter #(<= % 100000) (vals (first directory-sizes)))))
+
+(defn part-2
+  "Finds the size of the smallest directory whose deletion would ensure enough space for the update"
+  []
+  (let [space-used (get (first directory-sizes) "/")
+        space-needed (- 30000000 (- 70000000 space-used))]
+    (apply min (filter #(<= space-needed %) (vals (first directory-sizes))))))
+
+(let [part-1-answer (part-1)
+      part-2-answer (part-2)]
+  (println (str "Part 1: " part-1-answer))
+  (println (str "Part 2: " part-2-answer)))
+
+
 
